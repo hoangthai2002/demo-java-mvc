f4d25ce102f8543a625e3ca0667823d1b44bad77
hoangthai2002 <thaidepgiai2002@gmail.com>
2024-08-27 05:43:17 +0700

    update

---
+++ src/main/java/com/Demo_java_mvc/HelloController.java
@@ -0,0 +1,13 @@
+package com.Demo_java_mvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Helo wolddasdsad";
+    }
+}
