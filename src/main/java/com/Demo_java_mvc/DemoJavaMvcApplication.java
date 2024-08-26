97ea9e70c0c3a18d2f7691518381246b091059b8
hoangthai2002 <thaidepgiai2002@gmail.com>
2024-08-27 05:03:00 +0700

    demo

---
+++ src/main/java/com/Demo_java_mvc/DemoJavaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.Demo_java_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Demo_java_mvc/DemoJavaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Demo_java_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
